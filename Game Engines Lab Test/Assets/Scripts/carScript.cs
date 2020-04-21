4b1f9b3ec8822f8bd178c78af2d635367049c4a9
SlimShaney <44029427+SlimShaney@users.noreply.github.com>
2020-04-21 13:45:32 +0100

    Final Commit
    All requirements fully Implemented
    Sample video and readme update to follow

--- Game Engines Lab Test/Assets/Scripts/carScript.cs
+++ Game Engines Lab Test/Assets/Scripts/carScript.cs
@@ -5,12 +5,53 @@ using UnityEngine;
 public class carScript : MonoBehaviour
 {
 
-    private Renderer colourRenderer;
+    private Renderer _colourRenderer;
+
+    private gameManagement _managerScript;
+    private GameObject _target;
+    public Renderer lightColour;
+
+    private Vector3 velocity;
+    private float maxVelocity = 3;
+    private float maxForce = 1;
     
     void Start()
     {
-        colourRenderer = GetComponent<MeshRenderer>();
-        colourRenderer.material.color = Color.magenta;
+        _colourRenderer = GetComponent<MeshRenderer>();
+        _colourRenderer.material.color = Color.magenta;
+        _managerScript = FindObjectOfType<gameManagement>();
+        velocity = Vector3.zero;
+        TargetSelect();
     }
 
+    void Update()
+    {
+        if (lightColour.material.color != Color.green || 
+            (Vector3.Distance(this.transform.position, _target.transform.position) <= 0.2))
+        {
+            TargetSelect();
+        }
+        MoveToTarget();
+        
+        
+    }
+
+    void TargetSelect()
+    {
+        _target = _managerScript.trafficLights[Random.Range(0, _managerScript.trafficLights.Length)];
+        lightColour = _target.GetComponent<Renderer>();
+    }
+
+    void MoveToTarget()
+    {
+        var desiredVelocity = _target.transform.position - transform.position;
+        desiredVelocity = desiredVelocity.normalized * maxVelocity;
+
+        var steering = desiredVelocity - velocity;
+        steering = Vector3.ClampMagnitude(steering, maxForce);
+
+        velocity = Vector3.ClampMagnitude(velocity + steering, maxVelocity);
+        transform.position += velocity * Time.deltaTime;
+        transform.forward = velocity.normalized;        
+    }
 }
---
+++ Game Engines Lab Test/Assets/Scripts/gameManagement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManagement : MonoBehaviour
+{
+    public GameObject[] trafficLights = new GameObject[10];
+    
+    void Start()
+    {
+        CreateTrafficLights();
+        CreateCar();
+    }
+
+    void CreateTrafficLights()
+    {
+        for (int i = 0; i < trafficLights.Length; i++)
+        {
+            GameObject trafficLightInstance = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+            trafficLightInstance.AddComponent<lightscript>();
+            trafficLightInstance.transform.parent = transform;
+            
+            trafficLightInstance.name = "Traffic Light #" + (i + 1);
+            transform.eulerAngles = new Vector3(0, 360/trafficLights.Length * i, 0);
+            trafficLightInstance.transform.position = Vector3.forward * 10;
+            trafficLights[i] = trafficLightInstance;            
+        }
+    }
+
+    void CreateCar()
+    {
+        GameObject carInstance = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        carInstance.transform.localScale = new Vector3(1, 1, 1.5f);
+        carInstance.AddComponent<carScript>();
+        carInstance.name = "Car";
+    }
+}
